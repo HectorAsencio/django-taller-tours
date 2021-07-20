645142ae09a006a4fe8bdf87e9ba5d4b4187e6f9
Héctor Asencio <hector.asencio.lx@gmail.com>
2021-07-20 19:00:47 -0400

    Uso de base de datos en vista, envio de mensajes

--- tours/views.py
+++ tours/views.py
@@ -10,32 +10,7 @@ class IndexView(TemplateView):
 
 class ToursView(TemplateView):
     def get(self, request, **kwargs):
-        Tours = [
-            {
-                'id': '1',
-                'nombre': 'Cerros de Valparaíso',
-                'dias': '1',
-                'precio': '15000'
-            },
-            {
-                'id': '2',
-                'nombre': 'Valle del Elqui',
-                'dias': '2',
-                'precio': '35000'
-            },
-            {
-                'id': '3',
-                'nombre': 'Torres del Paine',
-                'dias': '9',
-                'precio': '450000'
-            },
-            {
-                'id': '4',
-                'nombre': 'Iglesias de Chiloé',
-                'dias': '3',
-                'precio': '59000'
-            },
-        ]
+        Tours = Tour.objects.all()
         context = {
             'title': 'Listado de Tours',
             'tours': Tours
